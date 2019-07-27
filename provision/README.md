4829f7c87294144d4338437db3a28e8184658bc0
josd <josderoo@gmail.com>
2019-07-27 21:04:10 +0200

    removed

--- provision/README.md
+++
@@ -1,6 +0,0 @@
-# EYE provision
-
-A collection of concrete input for EYE reasoning which can be generated via
-```
-./provision
-```
--- provision/euler/euler.py
+++
@@ -1,6 +0,0 @@
-from sympy import *
-
-if __name__ == "__main__":
-    print('PREFIX : <http://josd.github.io/eye/provision/euler#>')
-    print('')
-    print('[] :euler-identity "E**(I*pi) + 1 = %d".' % (E**(I*pi) + 1))
