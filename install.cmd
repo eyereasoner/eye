0bf80aca255766b0cbf3c6ffe934198438617eb5
josd <josderoo@gmail.com>
2019-09-04 23:45:00 +0200

    using .pl file extension for prolog files

--- install.cmd
+++ install.cmd
@@ -3,10 +3,10 @@ REM --------------------------------------
 REM installing EYE in C:\Program Files\eye
 REM --------------------------------------
 if not exist "C:\Program Files\eye\src" mkdir "C:\Program Files\eye\src"
-copy /Y "%~dp0eye.prolog" "C:\Program Files\eye\src"
+copy /Y "%~dp0eye.pl" "C:\Program Files\eye\src"
 if not exist "C:\Program Files\eye\lib" mkdir "C:\Program Files\eye\lib"
 pushd "C:\Program Files\eye\lib"
-swipl -q -f ../src/eye.prolog -g main -- --image eye.pvm
+swipl -q -f ../src/eye.pl -g main -- --image eye.pvm
 popd
 if not exist "C:\Program Files\eye\bin" mkdir "C:\Program Files\eye\bin"
 copy /Y "%~dp0eye.cmd" "C:\Program Files\eye\bin"
