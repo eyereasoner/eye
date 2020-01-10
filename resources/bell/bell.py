59d2fb4df9abd922e278c0610fde94702554ae48
josd <josderoo@gmail.com>
2020-01-10 11:50:26 +0100

    adding resources

---
+++ resources/bell/bell.py
@@ -0,0 +1,7 @@
+from sympy import *
+
+if __name__ == "__main__":
+    x = Symbol('x')
+    print('@prefix : <http://josd.github.io/eye/resources#>.')
+    print('')
+    print('[] :bell-integral "integrate(exp(-x**2), (x, -oo, oo)) = %s".' % (integrate(exp(-x**2), (x, -oo, oo))))
