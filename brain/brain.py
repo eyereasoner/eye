4839f68dde2e2b19d3d1d95cbd1cbdb23d4382b4
josd <josderoo@gmail.com>
2020-02-28 23:26:30 +0100

    refreshing

--- brain/brain.py
+++ brain/brain.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python3
-#
-# EYE python script
+# BRAnches of INtelligence - BRAIN -- Jos De Roo
 #
 # N3 proof engine inspired by http://www.ibiblio.org/obp/py4fun/prolog/prolog3.py
 # and is with depth first search and euler path anti-looping detection.
 # Thanks to Tim Berners-Lee and Dan Connolly for inventing N3 and for creating N3
 # running code.
 #
-# Usage: python3 eye.py [--why] [--once] [--debug] triples
-# Test:  python3 eye.py --why http://josd.github.io/eye/scripting/tree.n3
+# Usage: python3 brain.py [--why] [--once] [--debug] triples
+# Test:  python3 brain.py --why http://josd.github.io/eye/brain/tree.n3
 #
 # Remark that the N3 that is actually understood is very limited:
 # qnames, () lists, facts and => rules on a single line and not yet [] ; , ^ ! ^^ @
@@ -416,10 +414,10 @@ def load(path, rules, goals, np):
         s = s.strip()
         if s == '': continue
         elif s[0] == '#': continue
-        elif s.find('PREFIX') != -1:
-            t = tokenize(s, ' ')
+        elif s.find('@prefix') != -1:
+            t = tokenize(s.strip('.'), ' ')
             if np.get(t[1]) and np[t[1]] != t[2]:
-                sys.stderr.write('#FAIL PREFIX %s %s\n'%(t[1], t[2]))
+                sys.stderr.write('#FAIL @prefix %s %s.\n'%(t[1], t[2]))
                 break
             else: np[t[1]] = t[2]
         elif s.find('=> []') != -1:
@@ -468,7 +466,7 @@ def run(args):
         if debug: print('step %s %s %s' % (step[0], rule, env))
 
     for pfx, i in np.items():
-        print("PREFIX %s %s" % (pfx, i))
+        print("@prefix %s %s." % (pfx, i))
     print()
     for g in goals:
         print(prove(g, rules, why, once, count))
@@ -488,5 +486,5 @@ if __name__ == '__main__':
         sys.exit(0)
 
     if len(sys.argv) == 1:
-        print('Usage: eye.py [--why] [--once] [--debug] triples')
+        print('Usage: brain.py [--why] [--once] [--debug] triples')
     else: run(sys.argv[1:])
