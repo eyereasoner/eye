17f6010f0d9ca571c8393a3dda5013000461b44f
josd <josderoo@gmail.com>
2020-03-21 16:38:08 +0100

    refreshing

--- lamp/sle.py
+++ lamp/sle.py
@@ -3,10 +3,10 @@
 import numpy as np
 
 if __name__ == "__main__":
-    print("# main of 4eq.py")
+    print("# main of sle.py")
 
     A = np.array([[1, 1, 0, 0], [0, 0, 1, 1], [0.07, 0, -0.93, 0], [0, -0.89, 0, 0.11]])
     B = np.array([856, 1308, 0, 0])
     X = np.linalg.inv(A).dot(B)
-    print('4eq_1 = %s' % [int(round(x)) for x in X])
+    print('sle_solution_1 = %s' % [int(round(x)) for x in X])
     print("")
