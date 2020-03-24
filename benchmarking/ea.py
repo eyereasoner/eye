3bee3cb92adbee4d884a31cadad4abbcec906e5d
josd <josderoo@gmail.com>
2020-03-24 23:15:11 +0100

    refreshing

--- benchmarking/ea.py
+++ benchmarking/ea.py
@@ -3,22 +3,24 @@
 
 from random import choice, random
  
-target  = list("METHINKS IT IS LIKE A WEASEL")
-alphabet = " ABCDEFGHIJLKLMNOPQRSTUVWXYZ"
-p = 0.05 # mutation probability
-c = 100  # number of children in each generation
- 
 def neg_fitness(trial):
     return sum(t != h for t,h in zip(trial, target))
  
 def mutate(parent):
     return [(choice(alphabet) if random() < p else ch) for ch in parent]
  
-parent = [choice(alphabet) for _ in range(len(target))]
-i = 0
-while parent != target:
-    copies = (mutate(parent) for _ in range(c))
-    parent = min(copies, key=neg_fitness)
-    # print("%3d" % i)
-    i += 1
-print("".join(parent))
+if __name__ == "__main__":
+    print("# main of ea.py")
+    target  = list("METHINKS IT IS LIKE A WEASEL")
+    alphabet = " ABCDEFGHIJLKLMNOPQRSTUVWXYZ"
+    p = 0.05 # mutation probability
+    c = 100  # number of children in each generation
+    parent = [choice(alphabet) for _ in range(len(target))]
+    i = 0
+    while parent != target:
+        copies = (mutate(parent) for _ in range(c))
+        parent = min(copies, key=neg_fitness)
+        # print("%3d" % (i))
+        i += 1
+    print("ea_parent = '%s'" % ("".join(parent)))
+    print("")
