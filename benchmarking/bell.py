d372130df57c4e1eebbcd4f3a41612a351344597
josd <josderoo@gmail.com>
2020-03-28 11:47:11 +0100

    removing

--- benchmarking/bell.py
+++
@@ -1,9 +0,0 @@
-# See https://en.wikipedia.org/wiki/Gaussian_function
-
-from sympy import *
-
-if __name__ == "__main__":
-    print("# main of bell.py")
-    x = Symbol('x')
-    print("bell_integral = 'integrate(exp(-x**2), (x, -oo, oo)) = %s'" % (integrate(exp(-x**2), (x, -oo, oo))))
-    print("")
