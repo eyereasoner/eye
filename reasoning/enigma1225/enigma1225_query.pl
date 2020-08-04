5623ce12375e4a1ebfd41e4496f74e516d1a1cc8
josd <josderoo@gmail.com>
2020-08-04 18:51:16 +0200

    adding enigma1225

---
+++ reasoning/enigma1225/enigma1225_query.pl
@@ -0,0 +1,3 @@
+:-
+    '<http://josd.github.io/eye/reasoning/enigma1225#solve>'(10, Permutation, Board, Max),
+    write_term('<http://josd.github.io/eye/reasoning/enigma1225#solve>'(10, Permutation, Board, Max), [fullstop(true), nl(true), quoted(true)]).
