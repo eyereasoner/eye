ff468edadc971a5f0a6f7195c6cb99e193a1a82e
josd <josderoo@gmail.com>
2020-11-06 23:22:29 +0100

    misc

---
+++ looking/polygon.pl
@@ -0,0 +1,11 @@
+:- initialization(main).
+
+main :-
+    area([[4,2],[6,2],[7,6],[4,6],[5,5],[5,3],[4,2]], Area),
+    writeln(Area),
+    halt.
+
+area([_], 0).
+area([[A,B],[C,D]|E], F) :-
+    area([[C,D]|E], G),
+    F is (A*D-B*C)/2+G.
