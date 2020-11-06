23de3a452ffa02ee3c9cc739a934c25f29e804b9
josd <josderoo@gmail.com>
2020-11-07 00:17:17 +0100

    misc

---
+++ looking/ea.pl
@@ -0,0 +1,67 @@
+% Original code from https://rosettacode.org/wiki/Evolutionary_algorithm#Prolog
+
+:- initialization(main).
+
+main :-
+    srandom(11),
+    solve('METHINKS IT IS LIKE A WEASEL'),
+    halt.
+    
+solve(TargetAtom) :-
+    atom_codes(TargetAtom, Target),
+    length(Target, Len),
+    random_text(Len, Start),
+    evolve(0, 5, Target, Start).    % evolution 0 and 5% probability for a mutation
+
+random_text(0, []).                 % generate some random text (fixed length)
+random_text(Len, [H|T]) :-
+    succ(L, Len),
+    random_alpha(H),
+    random_text(L, T).
+
+random_alpha(Ch) :-                 % generate a single random character
+    P is random(27),
+    (   P = 0
+    ->  Ch is 32
+    ;   Ch is P+64
+    ).
+
+evolve(Evolution, Probability, Target, mutation(Score, Value)) :-
+    atom_codes(Val, Value),
+    writeln(Val),
+    (   Score = 0
+    ->  true
+    ;   evolve(Evolution, Probability, Target, Value)
+    ).
+evolve(Evolution, Probability, Target, Start) :-
+    findall(mutation(Score, M),     % generate 80 mutations, select the best
+        (   between(1, 80, _),
+            mutate(Probability, Start, M),
+            score(M, Score, Target)
+        ),
+        Mutations
+    ),
+    sort(Mutations, [Best|_]),
+    succ(Evolution, Evo),
+    evolve(Evo, Probability, Target, Best).
+
+mutate(_, [], []).                  % mutate(Probability, Input, Output)
+mutate(Probability, [H|Txt], [H|Mut]) :-
+    R is random(100),
+    R > Probability,
+    !,
+    mutate(Probability, Txt, Mut).
+mutate(Probability, [_|Txt], [M|Mut]) :-
+    random_alpha(M),
+    mutate(Probability, Txt, Mut).
+
+score(Txt, Score, Target) :-
+    score(Target, Txt, 0, Score).
+
+score([], [], Score, Score).        % score a generated mutation (count diffs)
+score([Ht|Tt], [Ht|Tp], C, Score) :-
+    !,
+    score(Tt, Tp, C, Score).
+score([_|Tt], [_|Tp], C, Score) :-
+    succ(C, N),
+    score(Tt, Tp, N, Score).
