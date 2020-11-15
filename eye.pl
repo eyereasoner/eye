6ab4169654968efc030fd9446b33a7b086b347ff
josd <josderoo@gmail.com>
2020-11-15 14:16:02 +0100

    fixing free variables as names starting with $

--- eye.pl
+++ eye.pl
@@ -23,27 +23,27 @@
 :- use_module(library(pcre)).
 :- use_module(library(date)).
 
-version_info('EYE v20.1115.1300 josd').
+version_info('EYE v20.1115.1316 josd').
 
 license_info('MIT License
 
 Copyright (c) 2009 Jos De Roo
 
-Permission is hereby granted,free of charge,to any person obtaining a copy
-of this software and associated documentation files (the "Software"),to deal
-in the Software without restriction,including without limitation the rights
-to use,copy,modify,merge,publish,distribute,sublicense,and/or sell
-copies of the Software,and to permit persons to whom the Software is
-furnished to do so,subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS",WITHOUT WARRANTY OF ANY KIND,EXPRESS OR
-IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,DAMAGES OR OTHER
-LIABILITY,WHETHER IN AN ACTION OF CONTRACT,TORT OR OTHERWISE,ARISING FROM,
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.').
 
@@ -51,7 +51,7 @@ help_info('Usage: eye <options>* <data>* <query>*
 eye
     swipl -x eye.pvm --
 <options>
-    --csv-separator <separator>     CSV separator such as ,or ;
+    --csv-separator <separator>     CSV separator such as , or ;
     --curl-http-header <field>      to pass HTTP header <field> to curl
     --debug                         output debug info on stderr
     --debug-cnt                     output debug info about counters on stderr
