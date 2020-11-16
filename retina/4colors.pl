7819910cb165d8ca42b20ad86a2a3d3ea4c5ca29
josd <josderoo@gmail.com>
2020-11-16 23:49:14 +0100

    misc

---
+++ retina/4colors.pl
@@ -0,0 +1,24 @@
+:- initialization(main).
+
+main :-
+    colors(Places),
+    Places = [p1-c4,p2-c3,p3-c2,p4-c1,p5-c1],
+    write('PASS'),
+    nl.
+
+colors(Places) :-
+    findall(Place-_,neighbours(Place,_),Places),
+    places(Places).
+
+places([]).
+places([Place-Color|Tail]) :-
+    places(Tail),
+    neighbours(Place,Neighbours),
+    member(Color,[c1,c2,c3,c4]),
+    \+ (member(Neighbour-Color,Tail),member(Neighbour,Neighbours)).
+
+neighbours(p1,[p2,p5,p4,p3]).
+neighbours(p2,[p1,p4,p3]).
+neighbours(p3,[p5,p1,p4,p2]).
+neighbours(p4,[p1,p2,p3]).
+neighbours(p5,[p1,p3]).
