8a6a3beb455342a8a2fe3b1f011b77d3d089519f
josd <josderoo@gmail.com>
2020-11-22 15:10:23 +0100

    misc [skip ci]

---
+++ retina/graph.pl
@@ -0,0 +1,35 @@
+:- initialization(main).
+
+main :-
+    findall(Path,path(paris,nantes,Path),Paths),
+    Paths = [[paris,orleans,blois,tours,lemans,angers,nantes],[paris,chartres,lemans,angers,nantes]],
+    write('[] = "PASS".'),
+    nl.
+
+oneway(paris,orleans).
+oneway(paris,chartres).
+oneway(paris,amiens).
+oneway(orleans,blois).
+oneway(orleans,bourges).
+oneway(blois,tours).
+oneway(chartres,lemans).
+oneway(lemans,angers).
+oneway(lemans,tours).
+oneway(angers,nantes).
+
+twoway(X,Y) :-
+    oneway(X,Y).
+twoway(X,Y) :-
+    oneway(Y,X).
+
+path(From,To,Path) :-
+    find(From,To,[From],Back),
+    reverse(Back,Path).
+
+find(From,To,Sofar,[To|Sofar]) :-
+    twoway(From,To).
+find(From,To,Sofar,Back) :-
+    twoway(From,Via),
+    Via \= To,
+    \+ member(Via,Sofar),
+    find(Via,To,[Via|Sofar],Back).
