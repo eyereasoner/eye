3607b95658e41da94f55f158d1f14bbb67ea9f72
josd <josderoo@gmail.com>
2020-11-22 23:32:22 +0100

    misc [skip ci]

---
+++ retina/curly.pl
@@ -0,0 +1,15 @@
+:-initialization(main).
+
+main :-
+    Item = {
+        author:'Philip K Dick',
+        works: [
+            {title:'The Man in the High Castle'},
+            {title:'Do Androids Dream of Electric Sheep'}
+       ]
+    },
+    Item = {Author,_},
+    Author = (author:V),
+    V = 'Philip K Dick',
+    write('[] = "PASS".'),
+    nl.
