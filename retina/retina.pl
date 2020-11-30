e58ebe0df0b8f77b391c02916064dc2711640c0c
josd <josderoo@gmail.com>
2020-11-30 01:10:40 +0100

    misc [skip ci]

--- retina/retina.pl
+++ retina/retina.pl
@@ -13,11 +13,16 @@ test :-
     nl.
 test.
 
+label(0).
+
 retina :-
     (   implies(Prem,Conc),
         call(Prem),
         \+call(Conc),
-        labelvars(Conc),
+        label(Current),
+        numbervars(Conc,Current,Next),
+        retractall(label(_)),
+        assertz(label(Next)),
         (   Conc = goal
         ->  true
         ;   astep(Conc),
@@ -29,45 +34,6 @@ retina :-
     ;   assertz(brake),
         retina
     ).
-
-label(0).
-
-labelvars(A) :-
-    label(B),
-    labelvars(A,B,C),
-    retractall(label(_)),
-    assertz(label(C)).
-
-labelvars(A,B,C) :-
-    var(A),
-    !,
-    number_chars(B,D),
-    atom_chars(E,D),
-    atom_concat('sk_',E,A),
-    C is B+1.
-labelvars(A,B,B) :-
-    atomic(A),
-    !.
-labelvars((A,B),C,D) :-
-    !,
-    labelvars(A,C,E),
-    labelvars(B,E,D).
-labelvars([A|B],C,D) :-
-    !,
-    labelvars(A,C,E),
-    labelvars(B,E,D).
-labelvars(A,B,C) :-
-    nonvar(A),
-    functor(A,_,D),
-    labelvars(0,D,A,B,C).
-
-labelvars(A,A,_,B,B) :-
-    !.
-labelvars(A,B,C,D,E) :-
-    F is A+1,
-    arg(F,C,G),
-    labelvars(G,D,H),
-    labelvars(F,B,C,H,E).
 
 astep((A,B)) :-
     assertz(A),
