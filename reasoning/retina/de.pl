cbb34853070cfdcb953626db6bfee78357bb8b7f
josd <josderoo@gmail.com>
2020-12-06 23:31:56 +0100

    misc [skip ci]

--- reasoning/retina/de.pl
+++ reasoning/retina/de.pl
@@ -2,7 +2,7 @@
 
 :- initialization(main).
 
-:- op(1150,xfx,'=>').
+:- op(1150,xfx,'-:').
 
 :- dynamic(saying/2).
 :- dynamic(not_saying/2).
@@ -10,7 +10,7 @@
 main :-
     [retina],
     % query implies goal
-    assertz((saying(_,'C') => goal)),
+    assertz((saying(_,'C') -: goal)),
     % assuming the negation of the query so that it can be discharged when the query succeeds
     assertz(not_saying(sk_0,'C')),
     retina,
@@ -19,13 +19,13 @@ main :-
     nl.
 
 % saying A implies saying C
-saying(S,'A') => saying(S,'C').
-not_saying(S,'C') => not_saying(S,'A').
+saying(S,'A') -: saying(S,'C').
+not_saying(S,'C') -: not_saying(S,'A').
 
 % saying B implies saying C
-saying(S,'B') => saying(S,'C').
-not_saying(S,'C') => not_saying(S,'B').
+saying(S,'B') -: saying(S,'C').
+not_saying(S,'C') -: not_saying(S,'B').
 
 % saying A or saying B
-not_saying(S,'A') => saying(S,'B').
-not_saying(S,'B') => saying(S,'A').
+not_saying(S,'A') -: saying(S,'B').
+not_saying(S,'B') -: saying(S,'A').
