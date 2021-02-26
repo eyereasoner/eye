98be11817f4328884fe39d349ca3184b73bf464f
josd <josderoo@gmail.com>
2021-02-26 21:34:50 +0100

    update INSTALL instructions

--- eye.pl
+++ eye.pl
@@ -22,7 +22,7 @@
 :- use_module(library(readutil)).
 :- use_module(library(prolog_jiti)).
 
-version_info('EYE v21.0226.1852 josd').
+version_info('EYE v21.0226.2034 josd').
 
 license_info('MIT License
 
