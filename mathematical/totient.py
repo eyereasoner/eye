b278fd747e22f3b8c9b05be15a55b18c6c108a45
josd <josderoo@gmail.com>
2021-04-12 00:50:40 +0200

    running with Python

--- mathematical/totient.py
+++ mathematical/totient.py
@@ -3,7 +3,8 @@
 from sympy import *
 
 if __name__ == "__main__":
-    print("totient(%d) = %d" % (2718, totient(2718)))
-    print("totient(%d) = %d" % (271828182, totient(271828182)))
+    print("totient(%d) = %d" % (271, totient(271)))
+    print("totient(%d) = %d" % (2718281, totient(2718281)))
+    print("totient(%d) = %d" % (27182818284, totient(27182818284)))
     print("totient(%d) = %d" % (271828182845904, totient(271828182845904)))
     print("")
