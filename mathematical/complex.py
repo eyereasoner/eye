f5628a3373b61351e1e5b23681d2069f35d4035d
josd <josderoo@gmail.com>
2021-04-11 14:29:07 +0200

    refreshing

---
+++ mathematical/complex.py
@@ -0,0 +1,9 @@
+# See https://en.wikipedia.org/wiki/Complex_number
+
+from sympy import *
+
+if __name__ == "__main__":
+    print("# running complex.py")
+    print("imaginary_unit = '(-1)**0.5 = %s'" % ((-1)**0.5))
+    print("euler_identity = 'E**(I*pi) + 1 = %d'" % (E**(I*pi) + 1))
+    print("")
--- mathematical/integral.py
+++ mathematical/integral.py
@@ -1,9 +1,10 @@
-# See https://en.wikipedia.org/wiki/Gaussian_function
+# See https://en.wikipedia.org/wiki/Integral
 
 from sympy import *
 
 if __name__ == "__main__":
-    print("# running bell.py")
+    print("# running integral.py")
     x = Symbol('x')
     print("bell_integral = 'integrate(exp(-x**2), (x, -oo, oo)) = %s'" % (integrate(exp(-x**2), (x, -oo, oo))))
+    print("bee1_integral = 'integrate(1/(1+x**2), (x, -oo, oo)) = %s'" % (integrate(1/(1+x**2), (x, -oo, oo))))
     print("")
