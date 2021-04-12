d3b52f3700c7675c8386d0a6987a1912b6c7ac69
josd <josderoo@gmail.com>
2021-04-12 12:35:38 +0200

    retesting

--- equations/fibonacci.py
+++ equations/fibonacci.py
@@ -7,6 +7,6 @@ def fibonacci(n, c = {0:1, 1:1}):
     return c[n]
 
 if __name__ == "__main__":
-    for i in [0, 1, 6, 91, 283, 3674, 29821, 754011]:
+    for i in [0, 1, 6, 91, 283, 3674, 29821]:
         print("fibonacci(%d) = %d" % (i, fibonacci(i)))
     print("")
