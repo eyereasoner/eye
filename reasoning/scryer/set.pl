3a95251c318ae53a8bdfda6ce0d62148fba7fdda
josd <josderoo@gmail.com>
2021-05-01 14:59:22 +0200

    adding set.pl

--- reasoning/scryer/set.pl
+++ reasoning/scryer/set.pl
@@ -1,17 +1,17 @@
 % See original code at https://samples.jbpub.com/9780763772062/PrologLabBook09.pdf
 
 % facts
-p(a, b).
-p(a, c).
-p(b, d).
-p(c, e).
+p(0, 1).
+p(0, 2).
+p(1, 3).
+p(2, 4).
 
 % test cases
-% _S = {x | ∃y p(x, y)} = {a, b, c}.
+% _S = {x | ∃y p(x, y)} = {0, 1, 2}.
 case((findall(p(X, Y), p(X, Y), _F), setof(X, Y^p(X, Y), _S))).
-% _S = {x | ∃y p(x, y)} = {a, b, c}.
-case((findall(p(X, Y), p(X, Y), _F), setof(X, p(a, X), _S))).
-% _S = {g(x, y) | ∃z (p(x, z) ∧ p(z, y))}.
+% _S = {x | ∃y p(x, y)} = {1, 2}.
+case((findall(p(X, Y), p(X, Y), _F), setof(X, p(0, X), _S))).
+% _S = {g(x, y) | ∃z (p(x, z) ∧ p(z, y))} = {g(0, 3), g(0, 4)}.
 case((findall(p(X, Y), p(X, Y), _F), setof(g(X, Y), Z^(p(X, Z), p(Z, Y)), _S))).
 
 test :-
