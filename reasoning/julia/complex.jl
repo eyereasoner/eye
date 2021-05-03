22836132783f964b00cfa3f1003fc8fc8aa66eca
josd <josderoo@gmail.com>
2021-05-04 01:11:34 +0200

    adding julia

---
+++ reasoning/julia/complex.jl
@@ -0,0 +1,1 @@
+println("[ :julia-statement \"exp(pi*im)+1 = ", exp(pi*im)+1, "\"].")
---
+++ reasoning/julia/fibonacci.jl
@@ -0,0 +1,3 @@
+fibonacci(n) = ([1 1 ; 1 0]^n)[1,2]
+
+println("[ :julia-statement \"fib(BigInt(3674)) = ", fibonacci(BigInt(3674)), "\"].")
