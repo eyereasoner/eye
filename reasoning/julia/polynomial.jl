5e202e27efa50a8ff5433c8e955ba9da9c064f4f
josd <josderoo@gmail.com>
2021-05-06 00:43:44 +0200

    adding polynomial.jl

---
+++ reasoning/julia/polynomial.jl
@@ -0,0 +1,4 @@
+using Polynomials
+
+println("[ :julia-statement \"roots(Polynomial([24+7im, -50, 35, -10, 1])) = ", roots(Polynomial([24+7im, -50, 35, -10, 1])),  "\"].")
+println("[ :julia-statement \"roots(Polynomial([-26, 24-44im, 14+33im, -9-5im, 1])) = ",  roots(Polynomial([-26, 24-44im, 14+33im, -9-5im, 1])), "\"].")
