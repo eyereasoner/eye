19fc5e30417f25d6964c1b1f48ecf46ed85dac09
josd <josderoo@gmail.com>
2021-05-09 20:19:39 +0200

    extending eye/reasoning

--- eye.pl
+++ eye.pl
@@ -23,7 +23,7 @@
 :- use_module(library(prolog_jiti)).
 :- use_module(library(http/http_open)).
 
-version_info('EYE v21.0414.2118 josd').
+version_info('EYE v21.0509.1819 josd').
 
 license_info('MIT License
 
