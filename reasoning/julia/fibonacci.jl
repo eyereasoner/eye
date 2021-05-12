73f0d1d9ffc763a0f4c3f70c57b640f390ffaf82
josd <josderoo@gmail.com>
2021-05-12 10:02:02 +0200

    refreshing

--- reasoning/julia/fibonacci.jl
+++ reasoning/julia/fibonacci.jl
@@ -3,6 +3,11 @@
 fibonacci(n) = ([1 1 ; 1 0] ^ n)[1, 2]
 
 cases = [
+    :(fibonacci(big(0)))
+    :(fibonacci(big(1)))
+    :(fibonacci(big(6)))
+    :(fibonacci(big(91)))
+    :(fibonacci(big(283)))
     :(fibonacci(big(3674)))
 ]
 
