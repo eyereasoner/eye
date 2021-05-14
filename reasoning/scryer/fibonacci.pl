dafcb4a26815930730bd932b4cff666d1f877a9e
josd <josderoo@gmail.com>
2021-05-14 11:55:57 +0200

    using clpz

--- reasoning/scryer/fibonacci.pl
+++ reasoning/scryer/fibonacci.pl
@@ -1,14 +1,16 @@
 % See https://en.wikipedia.org/wiki/Fibonacci_number
 
+:- use_module(library(clpz)).
+
 fibonacci(A, B) :-
     fibonacci(A, 0, 1, B).
 
 fibonacci(0, _, A, A).
 fibonacci(1, _, A, A).
 fibonacci(A, B, C, D) :-
-    A > 1,
-    E is A-1,
-    F is B+C,
+    A #> 1,
+    E #= A-1,
+    F #= B+C,
     fibonacci(E, C, F, D).
 
 % test cases
