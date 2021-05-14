2ec6d486fcded9bb6630fa90769ecafedd77be97
josd <josderoo@gmail.com>
2021-05-14 13:59:45 +0200

    refreshing

--- reasoning/trealla/sets.pl
+++ reasoning/trealla/sets.pl
@@ -7,12 +7,13 @@ p(1, 3).
 p(2, 4).
 
 % test cases
+case(findall(p(X, Y), p(X, Y), _F)).
 % _S = {x | ∃y p(x, y)} = {0, 1, 2}.
-case((findall(p(X, Y), p(X, Y), _F), setof(X, Y^p(X, Y), _S))).
+case(setof(X, Y^p(X, Y), _S)).
 % _S = {x | ∃y p(x, y)} = {1, 2}.
-case((findall(p(X, Y), p(X, Y), _F), setof(X, p(0, X), _S))).
+case(setof(X, p(0, X), _S)).
 % _S = {g(x, y) | ∃z (p(x, z) ∧ p(z, y))} = {g(0, 3), g(0, 4)}.
-case((findall(p(X, Y), p(X, Y), _F), setof(g(X, Y), Z^(p(X, Z), p(Z, Y)), _S))).
+case(setof(g(X, Y), Z^(p(X, Z), p(Z, Y)), _S)).
 
 test :-
     case(A),
