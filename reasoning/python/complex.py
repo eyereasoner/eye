f5dcea10964bc753191e820a481c7be34399e80e
josd <josderoo@gmail.com>
2021-05-24 22:43:46 +0200

    refreshing

---
+++ reasoning/python/complex.py
@@ -0,0 +1,18 @@
+# See https://en.wikipedia.org/wiki/Complex_number
+
+from sympy import sqrt, exp, log, I, pi, cos, sin
+from sympy.abc import x
+
+if __name__ == "__main__":
+    cases = [
+        "sqrt(-1)",
+        "sqrt(I)",
+        "exp(I*pi) + 1",
+        "log(I*x)",
+        "cos(I*x)",
+        "sin(I*x)"
+    ]
+
+    for c in cases:
+        print('[] :python-result "%s = %s".' % (c, eval(c)))
+    print("")
