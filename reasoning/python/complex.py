b62fd2d07eaf0885ea22df92d7dddedf9ab57b9f
josd <josderoo@gmail.com>
2021-06-29 23:04:12 +0200

    adding cases to complex

--- reasoning/python/complex.py
+++ reasoning/python/complex.py
@@ -1,16 +1,20 @@
 # See https://en.wikipedia.org/wiki/Complex_number
 
-from sympy import sqrt, exp, log, I, pi, cos, sin
+from sympy import sqrt, exp, log, I, pi, cos, sin, asin, N
 from sympy.abc import x
 
 if __name__ == "__main__":
     cases = [
         "sqrt(-1)",
-        "sqrt(I)",
         "exp(I*pi) + 1",
+        "log(-1)",
+        "log(-I)",
         "log(I*x)",
         "cos(I*x)",
-        "sin(I*x)"
+        "sin(I*x)",
+        "asin(I*x)",
+        "N(sqrt(I))",
+        "N(asin(2))"
     ]
 
     for c in cases:
