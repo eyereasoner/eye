f700454079aa48d1866a08bdff803e999c70c013
josd <josderoo@gmail.com>
2021-08-03 02:07:06 +0200

    using prolog to generate dt

---
+++ two/tmp/gendt.pl
@@ -0,0 +1,40 @@
+:- use_module(library(between)).
+:- use_module(library(format)).
+
+main :-
+    open('dt.pl',write,Out),
+    write(Out,'% Deep taxonomy\n'),
+    write(Out,'% See http://ruleml.org/WellnessRules/files/WellnessRulesN3-2009-11-10.pdf\n'),
+    write(Out,'\n'),
+    write(Out,'% type/2 comes from http://www.w3.org/1999/02/22-rdf-syntax-ns#type\n'),
+    write(Out,'type(z,class(n0)).\n'),
+    (   between(0,9999,I),
+        J is I+1,
+        format(Out,"type(X,class(n~w)) :- type(X,class(n~w)).~n",[J,I]),
+        format(Out,"type(X,class(i~w)) :- type(X,class(n~w)).~n",[J,I]),
+        format(Out,"type(X,class(j~w)) :- type(X,class(n~w)).~n",[J,I]),
+        %legacy_format(Out,"type(X,class(n~w)) :- type(X,class(n~w)).~n",[J,I]),
+        %legacy_format(Out,"type(X,class(i~w)) :- type(X,class(n~w)).~n",[J,I]),
+        %legacy_format(Out,"type(X,class(j~w)) :- type(X,class(n~w)).~n",[J,I]),
+        fail
+    ;   true
+    ),
+    write(Out,'\n'),
+    write(Out,'% test cases\n'),
+    write(Out,'case(type(_,class(n1))).\n'),
+    write(Out,'case(type(_,class(n10))).\n'),
+    write(Out,'case(type(_,class(n100))).\n'),
+    write(Out,'case(type(_,class(n1000))).\n'),
+    write(Out,'case(type(_,class(n10000))).\n'),
+    write(Out,'\n'),
+    write(Out,'test :-\n'),
+    write(Out,'    case(A),\n'),
+    write(Out,'    A,\n'),
+    write(Out,'    write(A),\n'),
+    write(Out,'    write(\'.\'),\n'),
+    write(Out,'    nl,\n'),
+    write(Out,'    fail.\n'),
+    write(Out,'test :-\n'),
+    write(Out,'    halt.\n'),
+    close(Out),
+    halt.
