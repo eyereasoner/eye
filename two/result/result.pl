d7ee3fa1c63de702905b4bf49406a29d89e063b0
josd <josderoo@gmail.com>
2021-08-05 23:41:34 +0200

    testing parallel sequences

--- two/result/result.pl
+++ two/result/result.pl
@@ -36,7 +36,8 @@ fibonacci(3674,29587295979710147947863436681515710810057321270525069057787104139
 
 testgcc([1,1,1,1,1,1,1,1,1],s(0,0,0),[s(0,0,1),s(0,1,1),s(0,1,0),s(1,1,0),s(1,1,1),s(1,0,1),s(1,0,0),s(0,0,0),s(0,0,1)]).
 
-findpath(location(i1,oostende),[drive_gent_brugge,drive_brugge_oostende],2400.0,0.01,0.9408,0.99,[3600.0,5.0,0.2,0.4,1]).
+findpath(location(i1,oostende),[drive_gent_brugge,drive_brugge_oostende],2400.0,0.01,0.9408,0.99,[5000.0,5.0,0.2,0.4,4]).
+findpath(location(i1,oostende),[drive_gent_kortrijk,drive_kortrijk_brugge,drive_brugge_oostende],4100.0,0.018,0.903168,0.9801,[5000.0,5.0,0.2,0.4,4]).
 
 path(angers,nantes).
 path(paris,nantes).
