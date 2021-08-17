7ceb0b1f7f5811867a409ae3b106f4731b53344c
josd <josderoo@gmail.com>
2021-08-18 01:45:26 +0200

    using web_nsp/2

--- two/complex.pl
+++ two/complex.pl
@@ -1,59 +1,61 @@
 % See https://en.wikipedia.org/wiki/Complex_number
 
-exp([A,B],[C,D],[E,F]) :-
-    polar([A,B],[R,T]),
+web_nsp(etc_,'http://josd.github.io/eye/two/cases#').
+
+etc_exp([A,B],[C,D],[E,F]) :-
+    etc_polar([A,B],[R,T]),
     E is R^C*exp(-D*T)*cos(D*log(R)+C*T),
     F is R^C*exp(-D*T)*sin(D*log(R)+C*T).
 
-polar([X,Y],[R,Tp]) :-
+etc_polar([X,Y],[R,Tp]) :-
     R is sqrt(X**2+Y**2),
     T is acos(abs(X)/R),
-    angular(X,Y,T,Tp).
+    etc_angular(X,Y,T,Tp).
 
-angular(X,Y,T,Tp) :-
+etc_angular(X,Y,T,Tp) :-
     X >= 0,
     Y >= 0,
     Tp = T.
-angular(X,Y,T,Tp) :-
+etc_angular(X,Y,T,Tp) :-
     X < 0,
     Y >= 0,
     Tp is pi-T.
-angular(X,Y,T,Tp) :-
+etc_angular(X,Y,T,Tp) :-
     X < 0,
     Y < 0,
     Tp is T+pi.
-angular(X,Y,T,Tp) :-
+etc_angular(X,Y,T,Tp) :-
     X >= 0,
     Y < 0,
     Tp is 2*pi-T.
 
-minus([X1,X2],[Y1,Y2]) :-
+etc_minus([X1,X2],[Y1,Y2]) :-
     Y1 is -X1,
     Y2 is -X2.
 
-minus([X1,X2],[Y1,Y2],[Z1,Z2]) :-
+etc_minus([X1,X2],[Y1,Y2],[Z1,Z2]) :-
     Z1 is X1-Y1,
     Z2 is X2-Y2.
 
-add([X1,X2],[Y1,Y2],[Z1,Z2]) :-
+etc_add([X1,X2],[Y1,Y2],[Z1,Z2]) :-
     Z1 is X1+Y1,
     Z2 is X2+Y2.
 
-times([X1,X2],[Y1,Y2],[Z1,Z2]) :-
+etc_times([X1,X2],[Y1,Y2],[Z1,Z2]) :-
     Z1 is X1*Y1-X2*Y2,
     Z2 is X1*Y2+X2*Y1.
 
-inverse([X1,X2],[Y1,Y2]) :-
+etc_inverse([X1,X2],[Y1,Y2]) :-
     Y1 is X1/(X1**2+X2**2),
     Y2 is -X2/(X1**2+X2**2).
 
-divide(X,Y,Z) :-
+etc_divide(X,Y,Z) :-
     inverse(Y,Yp),
-    times(X,Yp,Z).
+    etc_times(X,Yp,Z).
 
 % test cases
-case(exp([-1,0],[0.5,0],_ANSWER)).
-case(exp([e,0],[0,pi],_ANSWER)).
+case(etc_exp([-1,0],[0.5,0],_ANSWER)).
+case(etc_exp([e,0],[0,pi],_ANSWER)).
 
 test :-
     case(A),
