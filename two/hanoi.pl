a155b965c5ef351ecab9018489e81d0385641c68
josd <josderoo@gmail.com>
2021-08-18 01:42:28 +0200

    using web_nsp/2

--- two/hanoi.pl
+++ two/hanoi.pl
@@ -1,14 +1,16 @@
 % Towers of Hanoi
 
-move(0,_,_,_) :-
+web_nsp(etc_,'http://josd.github.io/eye/two/cases#').
+
+etc_move(0,_,_,_) :-
     !.
-move(N,A,B,C) :-
+etc_move(N,A,B,C) :-
     M is N-1,
-    move(M,A,C,B),
-    move(M,C,B,A).
+    etc_move(M,A,C,B),
+    etc_move(M,C,B,A).
 
 % test cases
-case(move(14,'left','centre','right')).
+case(etc_move(14,etc_left,etc_centre,etc_right)).
 
 test :-
     case(A),
