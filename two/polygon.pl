a12b6a02492bcf461df74c8904e601778d773628
josd <josderoo@gmail.com>
2021-08-18 01:46:45 +0200

    using web_nsp/2

--- two/polygon.pl
+++ two/polygon.pl
@@ -1,12 +1,14 @@
 % Calculating the area of a polygon
 
-area([_],0).
-area([[A,B],[C,D]|E],F) :-
-    area([[C,D]|E],G),
+web_nsp(etc_,'http://josd.github.io/eye/two/cases#').
+
+etc_area([_],0).
+etc_area([[A,B],[C,D]|E],F) :-
+    etc_area([[C,D]|E],G),
     F is (A*D-B*C)/2+G.
 
 % test cases
-case(area([[3,2],[6,2],[7,6],[4,6],[5,5],[5,3],[3,2]],_ANSWER)).
+case(etc_area([[3,2],[6,2],[7,6],[4,6],[5,5],[5,3],[3,2]],_ANSWER)).
 
 test :-
     case(A),
