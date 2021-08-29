3e4ebbace0e0d8eddbf23a3b225d86002091fe62
josd <josderoo@gmail.com>
2021-08-30 00:39:37 +0200

    using wrapper/2 to webize

--- lateral/socrates.pl
+++ lateral/socrates.pl
@@ -1,15 +1,16 @@
 % Socrates is a mortal
 
+wrapper(rdf,'http://www.w3.org/1999/02/22-rdf-syntax-ns#').
 wrapper(el,'https://josd.github.io/eye/lateral/ns#').
 
-man(socrates).
+rdf(type(el(socrates),el(human))).
 
-mortal(S) :-
-    man(S).
+rdf(type(S,el(mortal))) :-
+    rdf(type(S,el(human))).
 
 % test cases
 case(wrapper(_NS,_P)).
-case(mortal(_IND)).
+case(rdf(type(_IND,el(mortal)))).
 
 test :-
     case(A),
