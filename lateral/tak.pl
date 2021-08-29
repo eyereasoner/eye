e7bddc1c6acdd7cc0dc56554fc79325210e647cf
josd <josderoo@gmail.com>
2021-08-30 00:52:26 +0200

    using wrapper/2 to webize

--- lateral/tak.pl
+++ lateral/tak.pl
@@ -2,21 +2,21 @@
 
 wrapper(el,'https://josd.github.io/eye/lateral/ns#').
 
-tak([X,Y,Z],Z) :-
+el(tak([X,Y,Z],Z)) :-
     X =< Y,
     !.
-tak([X,Y,Z],A) :-
+el(tak([X,Y,Z],A)) :-
     X1 is X-1,
-    tak([X1,Y,Z],A1),
+    el(tak([X1,Y,Z],A1)),
     Y1 is Y-1,
-    tak([Y1,Z,X],A2),
+    el(tak([Y1,Z,X],A2)),
     Z1 is Z-1,
-    tak([Z1,X,Y],A3),
-    tak([A1,A2,A3],A).
+    el(tak([Z1,X,Y],A3)),
+    el(tak([A1,A2,A3],A)).
 
 % test cases
 case(wrapper(_NS,_P)).
-case(tak([34,13,8],_A)).
+case(el(tak([34,13,8],_A))).
 
 test :-
     case(A),
