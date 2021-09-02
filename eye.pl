5f71fccefcd69ea79b40f12ee3ef727d2bb72781
josd <josderoo@gmail.com>
2021-09-03 00:37:59 +0200

    reworking to EYE Thinking

--- eye.pl
+++ eye.pl
@@ -22,7 +22,7 @@
 :- use_module(library(prolog_jiti)).
 :- use_module(library(http/http_open)).
 
-version_info('EYE v21.0902.2232 josd').
+version_info('EYE v21.0902.2237 josd').
 
 license_info('MIT License
 
