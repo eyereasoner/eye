b5111273cd17e01215fe44652631bbd6efbbe2b8
josd <josderoo@gmail.com>
2021-09-12 21:52:27 +0200

    correcting log:collectAllIn built-in (obs from William Van Woensel)

--- eye.pl
+++ eye.pl
@@ -22,7 +22,7 @@
 :- use_module(library(prolog_jiti)).
 :- use_module(library(http/http_open)).
 
-version_info('EYE v21.0912.1949 josd').
+version_info('EYE v21.0912.1951 josd').
 
 license_info('MIT License
 
