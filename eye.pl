3317ee482bad660db2701b11b30f737b323a8359
josd <josderoo@gmail.com>
2021-09-12 22:42:42 +0200

    fixing bnodes in generated rules (obs from William Van Woensel)

--- eye.pl
+++ eye.pl
@@ -22,7 +22,7 @@
 :- use_module(library(prolog_jiti)).
 :- use_module(library(http/http_open)).
 
-version_info('EYE v21.0912.2031 josd').
+version_info('EYE v21.0912.2042 josd').
 
 license_info('MIT License
 
