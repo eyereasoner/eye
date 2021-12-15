dd54b4ca3ac45e2d630c335a2d858d71f9b01511
josd <josderoo@gmail.com>
2021-12-15 10:43:21 +0100

    refreshing

---
+++ reasoning/prolog/graph.pl
@@ -0,0 +1,20 @@
+% Traversing graph paths
+
+:- dynamic(path/2).
+
+oneway(paris,orleans).
+oneway(paris,chartres).
+oneway(paris,amiens).
+oneway(orleans,blois).
+oneway(orleans,bourges).
+oneway(blois,tours).
+oneway(chartres,lemans).
+oneway(lemans,angers).
+oneway(lemans,tours).
+oneway(angers,nantes).
+
+oneway(A,B) -: path(A,B).
+path(A,B),path(B,C) -: path(A,C).
+
+% query implies goal
+path(_CITY,nantes) -: goal.
