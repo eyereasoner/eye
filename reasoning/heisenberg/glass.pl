bb41c70064556b7d5fe536c57e7fce3603df9eb0
josd <josderoo@gmail.com>
2022-01-14 01:12:19 +0100

    retesting

--- reasoning/heisenberg/glass.pl
+++ reasoning/heisenberg/glass.pl
@@ -3,9 +3,9 @@
 :- use_module(library(iso_ext)).
 :- use_module(library(lists)).
 
-p([[a,b,c]],o).
-p([[r,s,t]],o).
-p([[r,s,t],[d,e,f],[r,s,t]],o).
+p([['#a','#b','#c']],'#o').
+p([['#r','#s','#t']],'#o').
+p([['#r','#s','#t'],['#d','#e','#f'],['#r','#s','#t']],'#o').
 
 q(A,B) :-
     p(C,B),
@@ -13,11 +13,11 @@ q(A,B) :-
     forall(member(E,C),member(E,A)).
 
 % query
-query(q([[_A,b,c]],o)).
-query(q([[a,B,C],[a,B,C]],o)).
-query(q([[a,B,C],[_A,B,C]],o)).
-query(q([[r,_S,t]],o)).
-query(q([[d,e,f],[r,_S,t]],o)).
-query(q([[d,_E,f],[r,s,t]],o)).
-query(q([[d,e,f],[r,s,t],[d,_E,f]],o)).
-query(q([[d,_E,f],[r,_S,t],[d,e,f]],o)).
+query(q([[_A,'#b','#c']],'#o')).
+query(q([['#a',B,C],['#a',B,C]],'#o')).
+query(q([['#a',B,C],[_A,B,C]],'#o')).
+query(q([['#r',_S,'#t']],'#o')).
+query(q([['#d','#e','#f'],['#r',_S,'#t']],'#o')).
+query(q([['#d',_E,'#f'],['#r','#s','#t']],'#o')).
+query(q([['#d','#e','#f'],['#r','#s','#t'],['#d',_E,'#f']],'#o')).
+query(q([['#d',_E,'#f'],['#r',_S,'#t'],['#d','#e','#f']],'#o')).
