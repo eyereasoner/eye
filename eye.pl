bad5afcb1c6439f93cd06899781db8d06043e727
josd <josderoo@gmail.com>
2022-01-25 15:45:22 +0100

    adding log:scope to deal with quantification in quoted graphs (obs from Doerthe Arndt)

--- eye.pl
+++ eye.pl
@@ -22,7 +22,7 @@
 :- use_module(library(prolog_jiti)).
 :- use_module(library(http/http_open)).
 
-version_info('EYE v22.0124.1659 josd').
+version_info('EYE v22.0125.1445 josd').
 
 license_info('MIT License
 
@@ -8746,6 +8746,15 @@ unify(A, B) :-
     !,
     getlist(A, C),
     C = B.
+unify(A, B) :-
+    nonvar(A),
+    nonvar(B),
+    A = '<http://www.w3.org/2000/10/swap/log#scope>'([Q|V], C),
+    !,
+    length(V, D),
+    length(E, D),
+    makevars([V,C], [F,G], V, E),
+    B = '<http://www.w3.org/2000/10/swap/log#scope>'([Q|F], G).
 unify(A, B) :-
     nonvar(A),
     nonvar(B),
