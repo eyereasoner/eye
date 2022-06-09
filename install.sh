45ed39da3f498a2108b916c65c214c4bade57c84
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-06-09 15:18:31 +0200

    INSTALL: Allow ./install.sh --prefix=$HOME.
    Also simplifies the installation a bit and removed the MacOS instructions
    as they seem the same as Linux while this also applies for similar systems such as *BSD.

--- install.sh
+++ install.sh
@@ -2,13 +2,35 @@
 # --------------------------
 # installing EYE in /opt/eye
 # --------------------------
-SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
-mkdir -p /opt/eye/src
-cp -a $SCRIPT_DIR/eye.pl /opt/eye/src
-mkdir -p /opt/eye/lib
-pushd /opt/eye/lib
-swipl -q -f ../src/eye.pl -g main -- --image eye.pvm
-popd
-mkdir -p /opt/eye/bin
-cp -a $SCRIPT_DIR/eye.sh /opt/eye/bin
-ln -sf /opt/eye/bin/eye.sh /usr/local/bin/eye
+
+prefix=/opt/eye
+
+usage()
+{ cat << _EOM_
+Install eye
+
+Usage: $0 [--prefix=dir]
+_EOM_
+}
+
+done=no
+while [ $done = no ]; do
+  case "$1" in
+    --prefix=*)
+      prefix="$(echo $1 | sed 's/[^=]*=//')"
+      shift
+      ;;
+    -*)
+      usage
+      exit 1
+      ;;
+    *)
+      done=yes
+  esac
+done
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+mkdir -p "$prefix/lib" "$prefix/bin" || exit 1
+swipl -q -f "$SCRIPT_DIR/eye.pl" -g main -- --image "$prefix/lib/eye.pvm" || exit 1
+sed "s#@PREFIX@#$prefix#g" "$SCRIPT_DIR/eye.sh.in" > "$prefix/bin/eye" || exit 1
+chmod 755 "$prefix/bin/eye"
