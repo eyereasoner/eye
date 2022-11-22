81a40100bc23cac76f79ed1b56d4910c5129b03c
josd <josderoo@gmail.com>
2022-11-22 11:48:02 +0100

    fixing owl:hasKey rule at https://josd.github.io/eye/reasoning/rpo/owl-hasKey.n3 (obs from Dörthe Arndt)

--- eye.pl
+++ eye.pl
@@ -19,7 +19,7 @@
 :- use_module(library(semweb/turtle)).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v22.1121.2131 josd').
+version_info('EYE v22.1122.1047 josd').
 
 license_info('MIT License
 
