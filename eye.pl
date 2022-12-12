18659be4c611c03696effc32bb5a97549ed1b71b
josd <josderoo@gmail.com>
2022-12-13 00:56:58 +0100

    refreshing

--- eye.pl
+++ eye.pl
@@ -360,7 +360,7 @@ gre(Argus) :-
     ;   A is random(2^62),
         atom_number(Genid, A)
     ),
-    atomic_list_concat(['http://josd.github.io/.well-known/genid/', Genid, '#'], Sns),
+    atomic_list_concat(['http://eyereasoner.github.io/.well-known/genid/', Genid, '#'], Sns),
     nb_setval(var_ns, Sns),
     (   (   flag(strings)
         ;   flag(image, _)
@@ -387,7 +387,7 @@ gre(Argus) :-
         ;   flag('pass-all-ground')
         )
     ->  retractall(pfx('var:', _)),
-        assertz(pfx('var:', '<http://josd.github.io/var#>'))
+        assertz(pfx('var:', '<http://eyereasoner.github.io/var#>'))
     ;   true
     ),
     (   flag('intermediate', Out)
@@ -459,7 +459,7 @@ gre(Argus) :-
         ),
         (   pfx('var:', _)
         ->  true
-        ;   assertz(pfx('var:', '<http://josd.github.io/var#>'))
+        ;   assertz(pfx('var:', '<http://eyereasoner.github.io/var#>'))
         ),
         (   pfx('skolem:', _)
         ->  true
@@ -2044,10 +2044,10 @@ pathitem(Name, []) -->
                 FD >= D,
                 \+flag('pass-all-ground')
             ->  atom_concat('_', N, Name)
-            ;   atomic_list_concat(['\'<http://josd.github.io/var#', N, '>\''], Name),
+            ;   atomic_list_concat(['\'<http://eyereasoner.github.io/var#', N, '>\''], Name),
                 (   pfx('var:', _)
                 ->  true
-                ;   assertz(pfx('var:', '<http://josd.github.io/var#>'))
+                ;   assertz(pfx('var:', '<http://eyereasoner.github.io/var#>'))
                 )
             )
         ;   (   quvar(S, N, D)
@@ -2055,10 +2055,10 @@ pathitem(Name, []) -->
                     FD >= D,
                     \+flag('pass-all-ground')
                 ->  atom_concat('_', N, Name)
-                ;   atomic_list_concat(['\'<http://josd.github.io/var#', N, '>\''], Name),
+                ;   atomic_list_concat(['\'<http://eyereasoner.github.io/var#', N, '>\''], Name),
                     (   pfx('var:', _)
                     ->  true
-                    ;   assertz(pfx('var:', '<http://josd.github.io/var#>'))
+                    ;   assertz(pfx('var:', '<http://eyereasoner.github.io/var#>'))
                     )
                 )
             ;   Name = S
@@ -3777,7 +3777,7 @@ wt0(X) :-
         ;   write('_:sk_')
         ),
         write(Y)
-    ;   atomic_list_concat(['<http://josd.github.io/var#some_', Y, '>'], Z),
+    ;   atomic_list_concat(['<http://eyereasoner.github.io/var#some_', Y, '>'], Z),
         wt0(Z)
     ).
 wt0(X) :-
@@ -3788,14 +3788,14 @@ wt0(X) :-
         \+flag('pass-all-ground')
     ->  write('?U_'),
         write(Y)
-    ;   atomic_list_concat(['<http://josd.github.io/var#all_', Y, '>'], Z),
+    ;   atomic_list_concat(['<http://eyereasoner.github.io/var#all_', Y, '>'], Z),
         wt0(Z)
     ).
 wt0(X) :-
     atom(X),
     atom_concat(avar, Y, X),
     !,
-    atomic_list_concat(['<http://josd.github.io/var#x_', Y, '>'], Z),
+    atomic_list_concat(['<http://eyereasoner.github.io/var#x_', Y, '>'], Z),
     wt0(Z).
 wt0(X) :-
     flag(nope),
@@ -4367,7 +4367,7 @@ wv(X) :-
     !,
     write('[ '),
     wp('<http://www.w3.org/2004/06/rei#uri>'),
-    write(' "http://josd.github.io/var#x_'),
+    write(' "http://eyereasoner.github.io/var#x_'),
     write(Y),
     write('"]').
 wv(X) :-
@@ -6651,7 +6651,7 @@ djiti_assertz(A) :-
             ->  nb_getval(var_ns, Sns),
                 atomic_list_concat(['<', Sns, 'sk_', V, '>'], U)
             ;   (   atom_concat(avar, V, X)
-                ->  atomic_list_concat(['<http://josd.github.io/var#x_', V, '>'], U)
+                ->  atomic_list_concat(['<http://eyereasoner.github.io/var#x_', V, '>'], U)
                 ;   U = X
                 )
             ),
@@ -11130,7 +11130,7 @@ shallowvars(_, [], _).
 
 findvar(A, alpha) :-
     !,
-    atom_concat('<http://josd.github.io/var#', _, A).
+    atom_concat('<http://eyereasoner.github.io/var#', _, A).
 findvar(A, beta) :-
     !,
     (   sub_atom(A, 0, _, _, '_bn_')
