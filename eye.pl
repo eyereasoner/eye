eb0b3f9723e54972be049a841771db1a56f0acce
josd <josderoo@gmail.com>
2022-12-24 01:45:05 +0100

    experimental implementation of -{ ... }- quoted graphs

--- eye.pl
+++ eye.pl
@@ -19,7 +19,7 @@
 :- use_module(library(semweb/turtle)).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v22.1223.2228 josd').
+version_info('EYE v22.1224.0044 josd').
 
 license_info('MIT License
 
