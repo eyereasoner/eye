4a83da1cdb26a7f268db403cb800e30312687bed
josd <josderoo@gmail.com>
2023-01-04 13:35:27 +0100

    doing eye realease via mkeye script

--- eye.pl
+++ eye.pl
@@ -19,7 +19,7 @@
 :- use_module(library(semweb/turtle)).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v23.0104.0019 josd').
+version_info('EYE v23.0104.1234 josd').
 
 license_info('MIT License
 
