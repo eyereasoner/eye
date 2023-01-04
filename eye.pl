30e2f0cfda292eefc09932e3973d91855f2ecd4c
josd <josderoo@gmail.com>
2023-01-04 15:40:47 +0100

    adding reasoning/blogic/socrates.n3p surfaces example

--- eye.pl
+++ eye.pl
@@ -19,7 +19,7 @@
 :- use_module(library(semweb/turtle)).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v23.0104.1432 josd').
+version_info('EYE v23.0104.1439 josd').
 
 license_info('MIT License
 
