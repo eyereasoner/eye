8d5dabf7ddd3f98f158574b3341c8d2fbe975bd4
josd <josderoo@gmail.com>
2023-02-23 16:01:25 +0100

    fixing log:notIncludes for (context recursion) scope (obs from Patrick Hochstenbach)

--- eye.pl
+++ eye.pl
@@ -20,7 +20,7 @@
 :- use_module(library(semweb/turtle)).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v3.1.1 josd').
+version_info('EYE v3.1.2 josd').
 
 license_info('MIT License
 
@@ -6779,7 +6779,8 @@ djiti_assertz(A) :-
         (   nonvar(X),
             nonvar(Y)
         ),
-        (   conj_list(X, A),
+        (   X \= [_, _],
+            conj_list(X, A),
             conj_list(Y, B),
             includes(A, B)
         )
@@ -6885,8 +6886,25 @@ djiti_assertz(A) :-
     ).
 
 '<http://www.w3.org/2000/10/swap/log#notIncludes>'(X, Y) :-
-    ignore(within_scope(X)),
-    \+'<http://www.w3.org/2000/10/swap/log#includes>'(X, Y).
+    within_scope(X),
+    !,
+    when(
+        (   nonvar(Y)
+        ),
+        (   \+call(Y)
+        )
+    ).
+'<http://www.w3.org/2000/10/swap/log#notIncludes>'(X, Y) :-
+    when(
+        (   nonvar(X),
+            nonvar(Y)
+        ),
+        (   X \= [_, _],
+            conj_list(X, A),
+            conj_list(Y, B),
+            \+includes(A, B)
+        )
+    ).
 
 '<http://www.w3.org/2000/10/swap/log#parsedAsN3>'(literal(A, _), B) :-
     atom_codes(A, C),
