082e1d94f1474cec974af357e0e9258bc93daa79
josd <josderoo@gmail.com>
2024-04-10 22:07:47 +0200

    command line arg --trig is now triggering lingua

--- eye.pl
+++ eye.pl
@@ -22,7 +22,7 @@
 :- catch(use_module(library(process)), _, true).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v10.2.6 (2024-04-10)').
+version_info('EYE v10.2.7 (2024-04-10)').
 
 license_info('MIT License
 
@@ -426,12 +426,7 @@ gre(Argus) :-
     args(Args),
 
     % lingua
-    (   (   '<http://www.w3.org/2000/10/swap/log#implies>'(Subj, Obj)
-        ;   '<http://www.w3.org/2000/10/swap/log#isImpliedBy>'(Subj, Obj)
-        ;   '<http://www.w3.org/2000/10/swap/log#query>'(Subj, Obj)
-        ),
-        atomic(Subj),
-        atomic(Obj)
+    (   memberchk('--trig', Args)
     ->  retractall(flag(lingua)),
         assertz(flag(lingua)),
 
