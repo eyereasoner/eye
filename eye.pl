c93285a07b67b1f87290bea8d3cbf74a07953af4
josd <josderoo@gmail.com>
2024-06-09 11:12:55 +0200

    using =^ as shorthand for log:query

--- eye.pl
+++ eye.pl
@@ -22,7 +22,7 @@
 :- catch(use_module(library(process)), _, true).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v10.14.0 (2024-06-08)').
+version_info('EYE v10.14.1 (2024-06-09)').
 
 license_info('MIT License
 
@@ -2827,7 +2827,7 @@ verb('\'<http://www.w3.org/2000/10/swap/log#implies>\'', []) -->
     ['=', '>'],
     !.
 verb('\'<http://www.w3.org/2000/10/swap/log#query>\'', []) -->
-    ['|', '>'],
+    ['=', '^'],
     !.
 verb('\'<http://www.w3.org/2002/07/owl#sameAs>\'', []) -->
     ['='],
