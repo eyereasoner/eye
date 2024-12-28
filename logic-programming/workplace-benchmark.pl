1969807bfb1d1c485c8793bce796dabb99ee46ed
josd <josderoo@gmail.com>
2024-12-28 20:40:00 +0100

    refreshing

--- logic-programming/workplace-benchmark.pl
+++ logic-programming/workplace-benchmark.pl
@@ -19,17 +19,17 @@
     true.
 
 % rules to check if an action complies with deontic logic
-'<urn:example:complies>'(Person, true) :+
+'<urn:example:complies>'(Person, true) :-
     '<urn:example:does>'(Person, '<urn:example:work_related_task>'),
+    '<urn:example:does>'(Person, '<urn:example:log_off_at_end_of_shift>').
+
+'<urn:example:complies>'(Person, true) :-
     '<urn:example:does>'(Person, '<urn:example:log_off_at_end_of_shift>').
 
 '<urn:example:complies>'(Person, false) :+
     '<urn:example:does>'(Person, '<urn:example:work_related_task>'),
     stable(1),
     \+'<urn:example:does>'(Person, '<urn:example:log_off_at_end_of_shift>').
-
-'<urn:example:complies>'(Person, true) :+
-    '<urn:example:does>'(Person, '<urn:example:log_off_at_end_of_shift>').
 
 '<urn:example:complies>'(Person, false) :+
     '<urn:example:does>'(Person, '<urn:example:access_social_media>').
