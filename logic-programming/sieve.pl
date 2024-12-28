9c8b905a9cadbf51f02bbaab87c894929dbe49f7
josd <josderoo@gmail.com>
2024-12-28 23:23:41 +0100

    adding goldbach test

--- logic-programming/sieve.pl
+++ logic-programming/sieve.pl
@@ -3,28 +3,28 @@
 :- op(1200, xfx, :+).
 
 '<urn:example:primes>'(Limit, Ps) :-
-    range(2, Limit, Is), 
+    range(2, Limit, Is),
     sift(Is, Ps).
 
-range(Start, End, []) :- 
+range(Start, End, []) :-
     Start > End.
-range(Start, End, [Start|Rest]) :- 
-    Start =< End, 
-    Next is Start + 1, 
+range(Start, End, [Start|Rest]) :-
+    Start =< End,
+    Next is Start+1,
     range(Next, End, Rest).
 
 sift([], []).
 sift([I|Is], [I|Ps]) :-
-    remove(I, Is, New), 
+    remove(I, Is, New),
     sift(New, Ps).
 
 remove(_, [], []).
 remove(P, [I|Is], Nis) :-
-    0 is I mod P, 
+    0 is I mod P,
     remove(P, Is, Nis).
 remove(P, [I|Is], [I|Nis]) :-
-    X is I mod P, 
-    X \= 0, 
+    X is I mod P,
+    X \= 0,
     remove(P, Is, Nis).
 
 % query
