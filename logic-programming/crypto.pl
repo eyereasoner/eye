c876995cced8b8fff7c14366dcff3e542aa358a0
josd <josderoo@gmail.com>
2024-12-28 18:31:22 +0100

    adding crypto example

--- logic-programming/crypto.pl
+++ logic-programming/crypto.pl
@@ -1,7 +1,7 @@
 % Crypo tests
 
 % queries
-true :+ '<http://www.w3.org/2000/10/swap/crypto#md5>'(literal('blargh', _), _X).
-true :+ '<http://www.w3.org/2000/10/swap/crypto#sha>'(literal('blargh', _), _X).
-true :+ '<http://www.w3.org/2000/10/swap/crypto#sha256>'(literal('blargh', _), _X).
-true :+ '<http://www.w3.org/2000/10/swap/crypto#sha512>'(literal('blargh', _), _X).
+true :+ '<http://www.w3.org/2000/10/swap/crypto#md5>'(literal('blargh', _), _).
+true :+ '<http://www.w3.org/2000/10/swap/crypto#sha>'(literal('blargh', _), _).
+true :+ '<http://www.w3.org/2000/10/swap/crypto#sha256>'(literal('blargh', _), _).
+true :+ '<http://www.w3.org/2000/10/swap/crypto#sha512>'(literal('blargh', _), _).
