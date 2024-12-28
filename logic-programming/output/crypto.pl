71b7278eb9d46fc79af4e82bafd5eb652b0649ec
josd <josderoo@gmail.com>
2024-12-28 18:29:40 +0100

    adding crypto example

---
+++ logic-programming/output/crypto.pl
@@ -0,0 +1,6 @@
+:- op(1200, xfx, :+).
+
+answer('<http://www.w3.org/2000/10/swap/crypto#md5>'(literal(blargh, type('<http://www.w3.org/2001/XMLSchema#string>')), literal(ef15c9bd4c7836612b1567f4c8396726, type('<http://www.w3.org/2001/XMLSchema#string>')))).
+answer('<http://www.w3.org/2000/10/swap/crypto#sha>'(literal(blargh, type('<http://www.w3.org/2001/XMLSchema#string>')), literal(d1e670385f40ee942a059f949c761214872ac35f, type('<http://www.w3.org/2001/XMLSchema#string>')))).
+answer('<http://www.w3.org/2000/10/swap/crypto#sha256>'(literal(blargh, type('<http://www.w3.org/2001/XMLSchema#string>')), literal('0ac33512d18e20d5fa1fab32eb8bbbf0cdc7cb60f87e963f5792df15df8b02cd', type('<http://www.w3.org/2001/XMLSchema#string>')))).
+answer('<http://www.w3.org/2000/10/swap/crypto#sha512>'(literal(blargh, type('<http://www.w3.org/2001/XMLSchema#string>')), literal('4a59e80b6a5bfb4c8d8a592086a290b347c5d2b62418abe878cfa8037063ca268edeadd184bc3d54bf683584f294caf7a3109c90458b45f93d25587618070fa1', type('<http://www.w3.org/2001/XMLSchema#string>')))).
