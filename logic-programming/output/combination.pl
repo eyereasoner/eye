db14d7f46c2430ea4dac2a217e091ff7223037fe
josd <josderoo@gmail.com>
2024-12-28 22:49:14 +0100

    adding combination example

---
+++ logic-programming/output/combination.pl
@@ -0,0 +1,34 @@
+:- op(1200, xfx, :+).
+
+answer('<urn:example:combination>'(0, [1, 2, 3, 4, 5], [])).
+answer('<urn:example:combination>'(1, [1, 2, 3, 4, 5], [1])).
+answer('<urn:example:combination>'(1, [1, 2, 3, 4, 5], [2])).
+answer('<urn:example:combination>'(1, [1, 2, 3, 4, 5], [3])).
+answer('<urn:example:combination>'(1, [1, 2, 3, 4, 5], [4])).
+answer('<urn:example:combination>'(1, [1, 2, 3, 4, 5], [5])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [1, 2])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [1, 3])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [1, 4])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [1, 5])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [2, 3])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [2, 4])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [2, 5])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [3, 4])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [3, 5])).
+answer('<urn:example:combination>'(2, [1, 2, 3, 4, 5], [4, 5])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [1, 2, 3])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [1, 2, 4])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [1, 2, 5])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [1, 3, 4])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [1, 3, 5])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [1, 4, 5])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [2, 3, 4])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [2, 3, 5])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [2, 4, 5])).
+answer('<urn:example:combination>'(3, [1, 2, 3, 4, 5], [3, 4, 5])).
+answer('<urn:example:combination>'(4, [1, 2, 3, 4, 5], [1, 2, 3, 4])).
+answer('<urn:example:combination>'(4, [1, 2, 3, 4, 5], [1, 2, 3, 5])).
+answer('<urn:example:combination>'(4, [1, 2, 3, 4, 5], [1, 2, 4, 5])).
+answer('<urn:example:combination>'(4, [1, 2, 3, 4, 5], [1, 3, 4, 5])).
+answer('<urn:example:combination>'(4, [1, 2, 3, 4, 5], [2, 3, 4, 5])).
+answer('<urn:example:combination>'(5, [1, 2, 3, 4, 5], [1, 2, 3, 4, 5])).
