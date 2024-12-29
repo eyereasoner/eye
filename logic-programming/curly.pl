b2b6d45a0bae28b80dca64cbf5b002d03876957f
josd <josderoo@gmail.com>
2024-12-29 12:29:52 +0100

    adding curly example

---
+++ logic-programming/curly.pl
@@ -0,0 +1,18 @@
+% Using curly braces
+
+'<urn:example:test>'(V) :-
+    Item = {
+        author:'Philip K Dick',
+        works:[
+            title:'The Man in the High Castle',
+            title:'Do Androids Dream of Electric Sheep'
+        ]
+    },
+    Item = {
+        Author,
+        _
+    },
+    Author = author:V.
+
+% query
+true :+ '<urn:example:test>'(_).
---
+++ logic-programming/output/curly.pl
@@ -0,0 +1,3 @@
+:- op(1200, xfx, :+).
+
+answer('<urn:example:test>'('Philip K Dick')).
