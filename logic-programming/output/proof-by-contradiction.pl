acb4db5ad773227b8af4cd9468ac369c407e9b33
josd <josderoo@gmail.com>
2024-12-30 13:53:59 +0100

    adding proof by contradiction example

---
+++ logic-programming/output/proof-by-contradiction.pl
@@ -0,0 +1,4 @@
+:- op(1200, xfx, :+).
+
+% inference fuse, return code 2
+  fuse('<urn:example:Mortal>'('<urn:example:Socrates>')).
---
+++ logic-programming/proof-by-contradiction.pl
@@ -0,0 +1,12 @@
+% proof by contradiction
+% See https://en.wikipedia.org/wiki/Proof_by_contradiction
+
+% facts
+'<urn:example:Human>'('<urn:example:Socrates>').
+
+% all humans are mortal
+'<urn:example:Mortal>'(X) :+
+    '<urn:example:Human>'(X).
+
+% assert the negation of the query
+false :+ '<urn:example:Mortal>'(_X).
