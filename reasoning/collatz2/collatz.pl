13aba9a83b6491b15cb84d417c4097080655ab98
josd <josderoo@gmail.com>
2025-01-25 14:17:29 +0100

    refreshing

---
+++ reasoning/collatz2/collatz.pl
@@ -0,0 +1,17 @@
+% Collatz conjecture
+
+'urn:example:collatz'(N, L) :-
+    collatz(N, 1, L).
+
+collatz(N, N, [N]) :-
+    !.
+collatz(N0, N, [N0|J]) :-
+    (   0 =:= N0 rem 2
+    ->  N1 is N0 // 2
+    ;   N1 is 3 * N0 + 1
+    ),
+    collatz(N1, N, J).
+
+% query
+(true :+ 'urn:example:collatz'(N, _)) :-
+    between(1, 1024, N).
