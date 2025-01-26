459567746cbc9af6f7055ac0a6119affd9d0a5db
josd <josderoo@gmail.com>
2025-01-26 20:19:45 +0100

    refreshing

---
+++ reasoning/gps2/gps-data.pl
@@ -0,0 +1,55 @@
+% test data: partial map of Belgium
+
+:- dynamic('<urn:example:location>'/2).
+
+'<urn:example:description>'(
+    '<urn:example:map_be>',
+    [   '<urn:example:location>'(S, '<urn:example:gent>'),
+        true,
+        '<urn:example:location>'(S, '<urn:example:brugge>'),
+        '<urn:example:drive_gent_brugge>',
+        1500.0,
+        0.006,
+        0.96,
+        0.99
+    ]
+).
+'<urn:example:description>'(
+    '<urn:example:map_be>',
+    [   '<urn:example:location>'(S, '<urn:example:gent>'),
+        true,
+        '<urn:example:location>'(S, '<urn:example:kortrijk>'),
+        '<urn:example:drive_gent_kortrijk>',
+        1600.0,
+        0.007,
+        0.96,
+        0.99
+    ]
+).
+'<urn:example:description>'(
+    '<urn:example:map_be>',
+    [   '<urn:example:location>'(S, '<urn:example:kortrijk>'),
+        true,
+        '<urn:example:location>'(S, '<urn:example:brugge>'),
+        '<urn:example:drive_kortrijk_brugge>',
+        1600.0,
+        0.007,
+        0.96,
+        0.99
+    ]
+).
+'<urn:example:description>'(
+    '<urn:example:map_be>',
+    [   '<urn:example:location>'(S, '<urn:example:brugge>'),
+        true,
+        '<urn:example:location>'(S, '<urn:example:oostende>'),
+        '<urn:example:drive_brugge_oostende>',
+        900.0,
+        0.004,
+        0.98,
+        1.0
+    ]
+).
+
+% current state
+'<urn:example:location>'('<urn:example:i1>', '<urn:example:gent>').
---
+++ reasoning/gps2/gps-query.pl
@@ -0,0 +1,13 @@
+% query
+true :+
+    '<urn:example:findpath>'(
+        '<urn:example:map_be>',
+        [   '<urn:example:location>'(_SUBJECT, '<urn:example:oostende>'),
+            _PATH,
+            _DURATION,
+            _COST,
+            _BELIEF,
+            _COMFORT,
+            [5000.0, 5.0, 0.2, 0.4, 1]
+        ]
+    ).
