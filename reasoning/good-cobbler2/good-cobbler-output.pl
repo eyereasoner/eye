7988c215feeb50c309523f5b886cb2489325335a
josd <josderoo@gmail.com>
2025-01-27 01:10:51 +0100

    adding good-cobbler2

---
+++ reasoning/good-cobbler2/good-cobbler-output.pl
@@ -0,0 +1,7 @@
+:- op(1200, xfx, :+).
+
+answer(('<urn:example:is>'(_, '<urn:example:good>'('<urn:example:Cobbler>')):+true)).
+
+% proof steps
+step(('<urn:example:is>'(_, '<urn:example:good>'('<urn:example:Cobbler>')):+true), true, '<urn:example:is>'(sk_0, '<urn:example:good>'('<urn:example:Cobbler>'))).
+step((true:+('<urn:example:is>'(_, '<urn:example:good>'(_)):+true)), ('<urn:example:is>'(_, '<urn:example:good>'('<urn:example:Cobbler>')):+true), true).
---
+++ reasoning/good-cobbler2/good-cobbler.pl
@@ -0,0 +1,7 @@
+% Good Cobbler
+
+% some x is a good cobbler
+'<urn:example:is>'(_, '<urn:example:good>'('<urn:example:Cobbler>')) :+ true.
+
+% is there some x which is good at some y
+true :+ ('<urn:example:is>'(_, '<urn:example:good>'(_)) :+ true).
