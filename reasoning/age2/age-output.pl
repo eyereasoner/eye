1e180367382e684776628b09fe1b2f408ff49f99
josd <josderoo@gmail.com>
2025-01-29 22:43:56 +0100

    refreshing

--- reasoning/age2/age-output.pl
+++ reasoning/age2/age-output.pl
@@ -1,6 +1,6 @@
 :- op(1200, xfx, :+).
 
-answer('<urn:example:ageAbove>'('<urn:example:patH>', literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>')))).
+answer('<https://eyereasoner.github.io/ns#ageAbove>'(patH, literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>')))).
 
 % proof steps
-step((true:+'<urn:example:ageAbove>'(_, literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>')))), '<urn:example:ageAbove>'('<urn:example:patH>', literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>'))), true).
+step((true:+'<https://eyereasoner.github.io/ns#ageAbove>'(_, literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>')))), '<https://eyereasoner.github.io/ns#ageAbove>'(patH, literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>'))), true).
--- reasoning/age2/age.pl
+++ reasoning/age2/age.pl
@@ -1,14 +1,17 @@
 :- op(1200, xfx, :+).
 
+% context
+'<https://eyereasoner.github.io/ns#ageAbove>'(A, B) :- ageAbove(A, B).
+
 % person data
-'<urn:example:birthDay>'('<urn:example:patH>', literal('1944-08-21', type('<http://www.w3.org/2001/XMLSchema#date>'))).
+birthDay(patH, literal('1944-08-21', type('<http://www.w3.org/2001/XMLSchema#date>'))).
 
 % is the age of a person above some duration?
-'<urn:example:ageAbove>'(A, B) :-
-    '<urn:example:birthDay>'(A, C),
+ageAbove(A, B) :-
+    birthDay(A, C),
     '<http://www.w3.org/2000/10/swap/time#localTime>'(literal('', type('<http://www.w3.org/2001/XMLSchema#string>')), D),
     '<http://www.w3.org/2000/10/swap/math#difference>'([D, C], E),
     '<http://www.w3.org/2000/10/swap/math#greaterThan>'(E, B).
 
 % query
-true :+ '<urn:example:ageAbove>'(_, literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>'))).
+true :+ '<https://eyereasoner.github.io/ns#ageAbove>'(_, literal('P80Y', type('<http://www.w3.org/2001/XMLSchema#duration>'))).
