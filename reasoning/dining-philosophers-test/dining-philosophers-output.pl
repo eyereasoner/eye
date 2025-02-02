f74981a6ad0c595fddfe824f553e0eae439288ce
josd <josderoo@gmail.com>
2025-02-02 23:21:04 +0100

    refreshing

--- reasoning/dining-philosophers-test/dining-philosophers-output.pl
+++ reasoning/dining-philosophers-test/dining-philosophers-output.pl
@@ -1,23 +1,23 @@
-% '<https://eyereasoner.github.io/ns#person1>' thinking for 0.1 seconds
-% '<https://eyereasoner.github.io/ns#person3>' thinking for 0.15 seconds
-% '<https://eyereasoner.github.io/ns#person2>' thinking for 0.2 seconds
-% '<https://eyereasoner.github.io/ns#person1>' eating for 0.1 seconds
-% '<https://eyereasoner.github.io/ns#person5>' thinking for 0.25 seconds
-% '<https://eyereasoner.github.io/ns#person4>' thinking for 0.25 seconds
-% '<https://eyereasoner.github.io/ns#person3>' eating for 0.1 seconds
-% '<https://eyereasoner.github.io/ns#person5>' eating for 0.1 seconds
-% '<https://eyereasoner.github.io/ns#person2>' eating for 0.2 seconds
-% '<https://eyereasoner.github.io/ns#person4>' eating for 0.2 seconds
-% '<https://eyereasoner.github.io/ns#person1>' thinking for 0.1 seconds
-% '<https://eyereasoner.github.io/ns#person3>' thinking for 0.15 seconds
-% '<https://eyereasoner.github.io/ns#person1>' eating for 0.1 seconds
-% '<https://eyereasoner.github.io/ns#person2>' thinking for 0.2 seconds
-% '<https://eyereasoner.github.io/ns#person4>' thinking for 0.25 seconds
-% '<https://eyereasoner.github.io/ns#person5>' thinking for 0.25 seconds
-% '<https://eyereasoner.github.io/ns#person3>' eating for 0.1 seconds
-% '<https://eyereasoner.github.io/ns#person4>' eating for 0.2 seconds
-% '<https://eyereasoner.github.io/ns#person2>' eating for 0.2 seconds
-% '<https://eyereasoner.github.io/ns#person5>' eating for 0.1 seconds
+% '<https://eyereasoner.github.io/ns#person1>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person3>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person1>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person3>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person4>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person2>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person2>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person4>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person5>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person5>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person1>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person3>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person1>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person3>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person2>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person4>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person2>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person4>' eating and putting down
+% '<https://eyereasoner.github.io/ns#person5>' thinking and picking up
+% '<https://eyereasoner.github.io/ns#person5>' eating and putting down
 :- op(1200, xfx, :+).
 
 answer('<https://eyereasoner.github.io/ns#got>'('<https://eyereasoner.github.io/ns#all>', '<https://eyereasoner.github.io/ns#dinner>')).
