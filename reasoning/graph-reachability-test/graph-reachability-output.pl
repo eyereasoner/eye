f771e95b532e1a06d1554f04b2716858274b4809
josd <josderoo@gmail.com>
2025-02-05 14:50:23 +0100

    adding graph-reachability-test

---
+++ reasoning/graph-reachability-test/graph-reachability-output.pl
@@ -0,0 +1,10 @@
+:- op(1200, xfx, :+).
+
+answer('<https://eyereasoner.github.io/ns#isReachable>'(a, f)).
+answer(\+'<https://eyereasoner.github.io/ns#isReachable>'(b, e)).
+answer('<https://eyereasoner.github.io/ns#isReachable>'(c, g)).
+
+% proof steps
+step((true:+'<https://eyereasoner.github.io/ns#isReachable>'(a, f)), '<https://eyereasoner.github.io/ns#isReachable>'(a, f), true).
+step((true:+ \+'<https://eyereasoner.github.io/ns#isReachable>'(b, e)), \+'<https://eyereasoner.github.io/ns#isReachable>'(b, e), true).
+step((true:+'<https://eyereasoner.github.io/ns#isReachable>'(c, g)), '<https://eyereasoner.github.io/ns#isReachable>'(c, g), true).
