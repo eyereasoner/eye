7f5b6cc471f0b9d2a14f6c6f15346dccd7d50d43
josd <josderoo@gmail.com>
2025-02-16 00:15:04 +0100

    adding goldbach tests

---
+++ reasoning/goldbach-00002-test/goldbach-output.pl
@@ -0,0 +1,6 @@
+:- op(1200, xfx, :+).
+
+answer(\+'<https://eyereasoner.github.io/ns#goldbach>'(2, [_, _])).
+
+% proof steps
+step((true:+ \+'<https://eyereasoner.github.io/ns#goldbach>'(2, [_, _])), \+'<https://eyereasoner.github.io/ns#goldbach>'(2, [_, _]), true).
