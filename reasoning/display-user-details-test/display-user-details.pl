ae44dae41fa4eccce8b8b0d7e750d75a55fc2d95
josd <josderoo@gmail.com>
2025-02-15 13:04:25 +0100

    adding examples and test cases

---
+++ reasoning/display-user-details-test/display-user-details.pl
@@ -0,0 +1,38 @@
+% display user details
+
+:- op(1200, xfx, :+).
+
+% user's personal details
+'<urn:example:userDetails>'(user_1, first_name, 'Patrick').
+'<urn:example:userDetails>'(user_1, place, 'Brugge').
+'<urn:example:userDetails>'(user_1, birth_year, 1971).
+
+% determine whether a given combination is allowed
+'<urn:example:display>'(User, perm(Perm), not(Not)) :-
+    '<urn:example:userDetails>'(User, _, _),
+    findall(Det, '<urn:example:userDetails>'(User, _, Det), All),
+    (   combination(1, All, Perm)
+    ;   combination(2, All, Perm)
+    ),
+    set_difference(All, Perm, Not).
+
+% combination
+combination(0, _, []).
+combination(I, As, Bs) :-
+    I > 0,
+    select(B, As, Cs),
+    J is I-1,
+    combination(J, Cs, Ds),
+    sort([B|Ds], Bs).
+
+% set_difference
+set_difference([], _, []).
+set_difference([A|Bs], Cs, Ds) :-
+    (   memberchk(A, Cs)
+    ->  set_difference(Bs, Cs, Ds)
+    ;   Ds = [A|Es],
+        set_difference(Bs, Cs, Es)
+    ).
+
+% query
+true :+ '<urn:example:display>'(_, _, _).
---
+++ reasoning/flute-puzzle-test/flute-puzzle-output.pl
@@ -0,0 +1,6 @@
+:- op(1200, xfx, :+).
+
+answer('<urn:example:solution>'(sandy)).
+
+% proof steps
+step((true:+'<urn:example:solution>'(_)), '<urn:example:solution>'(sandy), true).
