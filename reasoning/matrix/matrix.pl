df70025c40ee6df2ae62d99fd2255c5537fcf1a2
josd <josderoo@gmail.com>
2025-02-25 23:02:23 +0100

    refreshing

--- reasoning/matrix/matrix.pl
+++ reasoning/matrix/matrix.pl
@@ -41,7 +41,7 @@ mult(A, B, C) :-
 % computes the determinant for a positive semi-definite matrix.
 % Uses the Cholenski decomposition
 % ==
-% :+ '<https://eyereasoner.github.io/ns#determinant>'([[2, -1, 0], [-1, 2, -1], [0, -1, 2]], D).
+% ?- '<https://eyereasoner.github.io/ns#determinant>'([[2, -1, 0], [-1, 2, -1], [0, -1, 2]], D).
 % D = 3.999999999999999.
 % ==
 '<https://eyereasoner.github.io/ns#determinant>'(A, Det) :-
@@ -57,7 +57,7 @@ prod(A, P0, P) :-
 % inversion of a positive semi-definite matrix. Uses the Cholenski
 % decomposition
 % ==
-% :+ '<https://eyereasoner.github.io/ns#matrix_inversion>'([[2, -1, 0], [-1, 2, -1], [0, -1, 2]], L).
+% ?- '<https://eyereasoner.github.io/ns#matrix_inversion>'([[2, -1, 0], [-1, 2, -1], [0, -1, 2]], L).
 % L = [[0.7499999999999999, 0.5000000000000001, 0.2500000000000001], [0.5000000000000001, 1.0000000000000004, 0.5000000000000002], [0.2500000000000001, 0.5000000000000002, 0.7500000000000001]].
 % ==
 '<https://eyereasoner.github.io/ns#matrix_inversion>'(A, B) :-
@@ -73,7 +73,7 @@ prod(A, P0, P) :-
 % http://www.mcs.csueastbay.edu/~malek/TeX/Triangle.pdf
 % code from
 % ==
-% :+ '<https://eyereasoner.github.io/ns#matrix_inv_triang>'([[2, 0, 0], [-1, 2, 0], [0, -1, 2]], L).
+% ?- '<https://eyereasoner.github.io/ns#matrix_inv_triang>'([[2, 0, 0], [-1, 2, 0], [0, -1, 2]], L).
 % L = [[0.5, 0.0, 0.0], [0.25, 0.5, 0.0], [0.125, 0.25, 0.5]].
 % ==
 '<https://eyereasoner.github.io/ns#matrix_inv_triang>'(L1, L2) :-
@@ -139,7 +139,7 @@ get_diag(N0, N, L, [H|R]) :-
 %
 %   X(N*P), Y(P*M), M(N*M)
 % ==
-% :+ '<https://eyereasoner.github.io/ns#matrix_multiply>'([[[1, 2], [3, 4], [5, 6]], [[1, 1, 1], [1, 1, 1]]], R).
+% ?- '<https://eyereasoner.github.io/ns#matrix_multiply>'([[[1, 2], [3, 4], [5, 6]], [[1, 1, 1], [1, 1, 1]]], R).
 % R = [[3, 3, 3], [7, 7, 7], [11, 11, 11]].
 % ==
 % code from http://stackoverflow.com/questions/34206275/matrix-multiplication-with-prolog
@@ -171,7 +171,7 @@ diff(A, B, C) :-
 
 %% '<https://eyereasoner.github.io/ns#matrix_sum>'([+A, +B], -C) is det
 % ==
-% :+ '<https://eyereasoner.github.io/ns#matrix_sum>'([[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4], [5, 6]]], M).
+% ?- '<https://eyereasoner.github.io/ns#matrix_sum>'([[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4], [5, 6]]], M).
 % ==
 '<https://eyereasoner.github.io/ns#matrix_sum>'([X, Y], S) :-
     maplist(maplist(sum), X, Y, S).
@@ -183,9 +183,9 @@ sum(A, B, C) :-
 % computes the Cholesky decomposition of a positive semi-definite matrix
 % code from https://rosettacode.org/wiki/Cholesky_decomposition#C
 % ==
-% :+ '<https://eyereasoner.github.io/ns#cholesky_decomposition>'([[25, 15, -5], [15, 18, 0], [-5, 0, 11]], L).
+% ?- '<https://eyereasoner.github.io/ns#cholesky_decomposition>'([[25, 15, -5], [15, 18, 0], [-5, 0, 11]], L).
 % L = [[5.0, 0, 0], [3.0, 3.0, 0], [-1.0, 1.0, 3.0]].
-% :+ '<https://eyereasoner.github.io/ns#cholesky_decomposition>'([[18, 22, 54, 42], [22, 70, 86, 62], [54, 86, 174, 134], [42, 62, 134, 106]], L).
+% ?- '<https://eyereasoner.github.io/ns#cholesky_decomposition>'([[18, 22, 54, 42], [22, 70, 86, 62], [54, 86, 174, 134], [42, 62, 134, 106]], L).
 % L = [[4.242640687119285, 0, 0, 0], [5.185449728701349, 6.565905201197403, 0, 0], [12.727922061357857, 3.0460384954008553, 1.6497422479090704, 0], [9.899494936611667, 1.624553864213788, 1.8497110052313648, 1.3926212476456026]].
 % ==
 '<https://eyereasoner.github.io/ns#cholesky_decomposition>'(A, L) :-
