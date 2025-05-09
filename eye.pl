377c613c9fd4963c04a4b62457dd5b259f9d43ed
josd <josderoo@gmail.com>
2025-05-09 22:57:23 +0200

    dropping CONSTRUCT WHERE

--- eye.pl
+++ eye.pl
@@ -23,7 +23,7 @@
 :- catch(use_module(library(process)), _, true).
 :- catch(use_module(library(http/http_open)), _, true).
 
-version_info('EYE v11.16.7 (2025-05-09)').
+version_info('EYE v11.16.8 (2025-05-09)').
 
 license_info('MIT License
 
@@ -95,8 +95,6 @@ eye
     --n3 <uri>                      N3 triples and rules
     --n3p <uri>                     N3P intermediate
     --proof <uri>                   N3 proof lemmas
-    --sparql-backward <uri>         SPARQL CONSTRUCT WHERE backward rules
-    --sparql-forward <uri>          SPARQL CONSTRUCT WHERE forward rules
     --trig <uri>                    TriG data
     --turtle <uri>                  Turtle triples
 <query>
@@ -377,8 +375,6 @@ argv([Arg|Argvs], [U, V|Argus]) :-
             '--quantify',
             '--query',
             '--skolem-genid',
-            '--sparql-backward',
-            '--sparql-forward',
             '--tactic',
             '--trig',
             '--turtle'
@@ -424,8 +420,6 @@ gre(Argus) :-
     nb_setval(doc_nr, 0),
     nb_setval(wn, 0),
     nb_setval(prepare, false),
-    nb_setval(sparql_backward, false),
-    nb_setval(prefix, false),
     opts(Argus, Args),
     (   Args = []
     ->  opts(['--help'], _)
@@ -915,8 +909,6 @@ opts([Arg|_], _) :-
             '--pass-all',
             '--proof',
             '--query',
-            '--sparql-backward',
-            '--sparql-forward',
             '--trig',
             '--turtle'
         ]
@@ -1080,36 +1072,6 @@ args(['--proof', Arg|Args]) :-
 args(['--query', Arg|Args]) :-
     !,
     n3_n3p(Arg, query),
-    args(Args).
-args(['--sparql-backward', Arg|Args]) :-
-    !,
-    absolute_uri(Arg, A),
-    atomic_list_concat(['<', A, '>'], R),
-    assertz(scope(R)),
-    (   flag(intermediate, Out)
-    ->  portray_clause(Out, scope(R))
-    ;   true
-    ),
-    nb_setval(sparql_backward, true),
-    n3_n3p(Arg, data),
-    nb_setval(sparql_backward, false),
-    nb_setval(fdepth, 0),
-    nb_setval(pdepth, 0),
-    nb_setval(cdepth, 0),
-    args(Args).
-args(['--sparql-forward', Arg|Args]) :-
-    !,
-    absolute_uri(Arg, A),
-    atomic_list_concat(['<', A, '>'], R),
-    assertz(scope(R)),
-    (   flag(intermediate, Out)
-    ->  portray_clause(Out, scope(R))
-    ;   true
-    ),
-    n3_n3p(Arg, data),
-    nb_setval(fdepth, 0),
-    nb_setval(pdepth, 0),
-    nb_setval(cdepth, 0),
     args(Args).
 args(['--trig', Argument|Args]) :-
     !,
@@ -2019,8 +1981,7 @@ declaration -->
         resolve_uri(U, V, URI),
         retractall(ns(Prefix, _)),
         assertz(ns(Prefix, URI)),
-        put_pfx(Prefix, URI),
-        nb_setval(prefix, true)
+        put_pfx(Prefix, URI)
     },
     withoutdot.
 
@@ -2525,33 +2486,6 @@ separate -->
 separate -->
     [].
 
-simpleStatement([Rule]) -->
-    [name(Construct)],
-    {   downcase_atom(Construct, 'construct')
-    },
-    !,
-    ['{'],
-    {   nb_getval(fdepth, I),
-        J is I+1,
-        nb_setval(fdepth, J)
-    },
-    formulacontent(Conc),
-    ['}'],
-    [name(Where)],
-    {   downcase_atom(Where, 'where')
-    },
-    ['{'],
-    formulacontent(Prem),
-    {   nb_setval(fdepth, I)
-    },
-    ['}'],
-    separate,
-    withoutdot,
-    {   (   nb_getval(sparql_backward, true)
-        ->  Rule = ':-'(Conc, Prem)
-        ;   Rule = '\'<http://www.w3.org/2000/10/swap/log#implies>\''(Prem, Conc)
-        )
-    }.
 simpleStatement(Quads) -->
     [name(Name)],
     {   downcase_atom(Name, 'graph')
@@ -3654,10 +3588,7 @@ wh :-
             (   pfx(A, B),
                 \+wpfx(A)
             ),
-            (   (   nb_getval(prefix, true)
-                ->  format("PREFIX ~w ~w~n", [A, B])
-                ;   format("@prefix ~w ~w.~n", [A, B])
-                ),
+            (   format("@prefix ~w ~w.~n", [A, B]),
                 assertz(wpfx(A)),
                 nb_setval(wpfx, true)
             )
